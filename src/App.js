62f9eacb2be28fae186bba3302b1583df2e7bea5
MohammedAlkhrashi <mohammad-x-@hotmail.com>
2021-02-16 02:56:07 +0300

    fierbase init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
+import firebase from "./firebase"
+import HomePageView from './views/homepage/HomePageView';
 
 function App() {
+  const [endDate,setEndDate] = useState(null)
+  // example; 
+  // const ref = firebase.firestore().collection("test") 
+  // ref.doc('test2').set({'ok':2})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <HomePageView/>
+      </div>
   );
 }
 
