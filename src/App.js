94ed45bbc6ad1fc3f395e0989f9cda90f1efcade
Your Name <store-_-888@hotmail.com>
2021-02-16 14:30:24 +0300

    init answer page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React, {useState} from 'react'
-import firebase from "./firebase"
-import HomePageView from './views/homepage/HomePageView';
+// import firebase from 'firebase'
 
+import AnswerPageView from './views/answerPgae/answerPageView';
 function App() {
   const [endDate,setEndDate] = useState(null)
 
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-
+    <AnswerPageView/>
 
       </header>
     </div>
