5e1b629ac93fce3696ddd49a721b86c8a4f3a85d
Your Name <store-_-888@hotmail.com>
2021-02-16 17:25:48 +0300

    signInAnonymously + update counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import firebase from "./firebase";
-import HomePageView from "./views/homepage/HomePageView";
+import HomePageView from "./views/homePage/homePageView";
 
 import AnswerPageView from './views/answerPgae/answerPageView';
 function App() {
   const [endDate, setEndDate] = useState(null);
+
+    const handleCounter = async () => {
+        const countDoc = firebase.firestore().collection("docs").doc("count");
+        const addOne = firebase.firestore.FieldValue.increment(1);
+        await countDoc.update({ userCount: addOne });
+    };
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged(firebaseUser => {
+            console.log(firebaseUser)
+            if (firebaseUser) {
+                alert("anon user");
+            } else {
+                firebase.auth().signInAnonymously();
+                handleCounter();
+                alert("new user");
+            }
+        });
+    },[]);
 
   return (
     <div className="App">
--- src/views/homePage/homePageView/index.js
+++ src/views/homePage/homePageView/index.js
@@ -1,19 +1,10 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Button } from "@material-ui/core";
 import { useState } from "react";
 import firebase from "firebase";
 
 const HomePageView = () => {
-  const handleClick = async () => {
-    const countDoc = firebase.firestore().collection("docs").doc("count");
-    const addOne = firebase.firestore.FieldValue.increment(1);
-    await countDoc.set({ userCount: addOne });
 
-    // next logic to implement:
-    // signInAnon here then go to the next page.
-    // if a user is signedInAnon he will be redircted to the answerPage
-    // for example if he closes and reopens the page he will be redircted to the answerPage.
-  };
 
   return (
     <div>
@@ -22,7 +13,7 @@ const HomePageView = () => {
         <h3>لقد وصلت</h3>
       </center>
       <Button
-        onClick={handleClick}
+        // onClick={handleClick}
         variant="outlined"
         size="large"
         color="inherit"
